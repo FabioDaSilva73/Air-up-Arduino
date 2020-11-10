31f8720ab7c624b13a3ee8b559e5f6da55b8d3f0
FabioDaSilva73 <fabiocastro231999@gmail.com>
2020-11-10 05:57:06 -0300

    teste

--- Codificação/testess.cpp
+++ Codificação/testess.cpp
@@ -1,6 +1,39 @@
 #include<bits/stdc++.h>
 
+///teste
+
+bool eh_primo(int x)
+{
+	if (x <= 3)
+	{
+		return true;		
+	}
+	
+	for (int i = 2; i <= x / 2 + 1; i++)
+	{
+		if (x % i == 0)
+		{
+			return false;
+		}
+	}
+	
+	return true;
+}
 
 int main(){
     std::cout << "Update" << std::endl;
+
+	int x;
+	
+	std::cin >> x;
+
+	if (eh_primo(x) == true)
+	{
+		std::cout << "S" << std::endl;
+	}
+	
+	else
+	{
+        std::cout << "N" << std::endl;;
+    }
 }
